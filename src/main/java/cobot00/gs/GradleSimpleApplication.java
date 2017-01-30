48161d22aab2fe48bb763f08deaca31f4fa6716d
Ryoji Kobori <kobori75@gmail.com>
2017-01-30 22:58:23 +0900

    add 3rd layer

--- src/main/java/cobot00/gs/GradleSimpleApplication.java
+++ src/main/java/cobot00/gs/GradleSimpleApplication.java
@@ -2,6 +2,7 @@ package cobot00.gs;
 
 import cobot00.gs.first.FirstLayer;
 import cobot00.gs.first.second.SecondLayer;
+import cobot00.gs.first.second.third.ThirdLayer;
 
 public class GradleSimpleApplication {
 
@@ -16,5 +17,8 @@ public class GradleSimpleApplication {
 
         SecondLayer second = new SecondLayer(2, "Gradle");
         second.log();
+
+        ThirdLayer third = new ThirdLayer(3, "YAML");
+        third.log();
     }
 }
